9edf9b2f13877d84d677ab76863fc0c1b0870b2d
Joshua Johnson <thingster@gmail.com>
2015-10-09 22:52:15 -0700

    Swapping PC's

---
+++ app/src/main/java/watch/fight/android/fightbrowser/Config/PreferencesActivity.java
@@ -0,0 +1,12 @@
+package watch.fight.android.fightbrowser.Config;
+
+/**
+ * Created by Joshua on 10/9/15.
+ */
+public class PreferencesActivity {
+    // Text bind to switch toggle w/ boolean
+    // onToggle
+    // onSlide
+    // Type - Toggle / Slide / Delete
+
+}
